81996f07eed8e447c26f80b6cb6685f3b97b8600
TahaHamdy-MernDev <TahaHamdy.mern@gmail.com>
2026-03-10 23:06:10 +0200

    Configure tsup to externalize UI dependencies, add 'use client' banner, and set `sideEffects` to false for optimized bundling.

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,8 +5,22 @@ export default defineConfig({
   format: ["cjs", "esm"],
   dts: true,
   clean: true,
-  external: ["react", "react-dom", "react-hook-form", "tailwindcss"],
+  external: [
+    "react",
+    "react-dom",
+    "react-hook-form",
+    "tailwindcss",
+    "clsx",
+    "date-fns",
+    "lucide-react",
+    "react-day-picker",
+    "tailwind-merge",
+    "zod",
+  ],
   treeshake: true,
   sourcemap: true,
   minify: true,
+  banner: {
+    js: '"use client";',
+  },
 });
